cda88a49d38dbfe58678ec34f8cd78e592daa8f1
Ehor Meschenko <meshhenko04@bk.ru>
2023-06-15 18:11:43 +0300

    Improvement of the praser, now I can parse all articles

--- main.py
+++ main.py
@@ -29,40 +29,44 @@ import lzma
 import base64
 import lxml
 import lxml.html
+import requests
 
 
 def get_all_articles__yahoo_finance():
     url = 'https://finance.yahoo.com'
-
-    page = urlopen(url)
-
-    html_ = page.read()
-    print('get1')
+    str_ = 'https://finance.yahoo.com/news/'
+    html_ = requests.get(url).content
     soup = BeautifulSoup(html_, 'html.parser')
-    print(soup)
     links = set()
-    print('get')
     for link in soup.find_all('a'):
         l = link.get('href')
         if l != None and l.startswith('https'):
-            print(l)
-            links.add(l)
+            if (len(str(l)) > 31):
+                if (str_ == str(l)[:31]):
+                    print(l)
+                    links.add(l)
     return links
 #return links
 def get_article_info(url):
-    page = urlopen(url)
-    html_ = page.read()
-    soup = BeautifulSoup(html_, 'lxml')
-    print(soup)
-    print('hhhhhhhhhhhhhhhhhhhhhhhhhhhh')
-    atribute_ = soup.find("h1", class_="").text
-    print(atribute_)
-    return atribute_
+    # page = urlopen(url)
+    # html_ = page.read()
+    # html_ = requests.get(url).content
+    # soup = BeautifulSoup(html_, 'html.parser')
+    # atribute_ = soup.find('article')
+    # return atribute_
+    j = 1
+    ch = str(url)[len(str(url))-j]
+    while ch != '/':
+        j += 1
+        ch = str(url)[len(str(url))-j]
+    article_ = str(url)[-(j-1):-5]
+    return str(url)[-(j-1):-5]
 #return atribute_ не знаю зачем, если я могу получить html код страницы во время обычного парсинга,
 # но если возвращать не str(soup), а atribute_ то в этом есть какой то смысл
 def parse_article__yahoo_finance(url):
-    page = urlopen(url)
-    html_ = page.read().decode('utf-8')
+    # page = urlopen(url)
+    # html_ = page.read().decode('utf-8')
+    html_ = requests.get(url).content
     soup = BeautifulSoup(html_, 'html.parser')
     atribute_ = soup.find("h1", class_="").text
     publication_dt_ = converter.parse(soup.find("time").text).isoformat()
@@ -82,10 +86,10 @@ def parse_article__yahoo_finance(url):
     )
     return ArticleInfo
 #return ArticleInfo
-def save_to_disk(atribute_, ArticleInfo):
+def save_to_disk(article_, ArticleInfo):
     try:
-        os.makedirs(atribute_)
-        with lzma.open(atribute_ + "/" + atribute_ + ".xz", "w") as file:
+        os.makedirs(article_)
+        with lzma.open(article_ + '/' + article_ + '.xz', "w") as file:
             file.write(ArticleInfo[4].encode('utf-8'))
             file.close()
         jsonFile = {
@@ -97,7 +101,7 @@ def save_to_disk(atribute_, ArticleInfo):
             'language': ArticleInfo[6]
         }
         #print(jsonFile)
-        with lzma.open(atribute_ + "/" + "json" + atribute_ +".xz", "w") as file:
+        with lzma.open(article_ + '/' + 'json' + article_ + '.xz', "w") as file:
              file.write(str(jsonFile).encode('utf-8'))
              file.close()
              #print('t')
@@ -105,57 +109,50 @@ def save_to_disk(atribute_, ArticleInfo):
         # directory already exists
         pass
 #save
-# print("start")
-# print("getting links")
-#
-# links = set()
-# links = get_all_articles__yahoo_finance()
-# for link in links:
-#     print (link)
-# print("ok")
-# # for link in links:
-# #     print(" parsing start " + link)
-# #     print(" getting atribute_")
-# #     atribute_ = get_article_info(link)
-# #     if (ArticleInfo == 'NoneType'):
-# #         print("ArticleInfo has invalid argument")
-# #         continue
-# #     atribute_ = base64.urlsafe_b64encode(atribute_)
-# #     print(" ok")
-# #     print(" getting ArticleInfo")
-# #     ArticleInfo = parse_article__yahoo_finance(link)
-# #     if (ArticleInfo == 0):
-# #         print("ArticleInfo has invalid argument")
-# #         continue
-# #     print(" ok")
-# #     print(" saving file")
-# #     save_to_disk(atribute_, ArticleInfo)
-# #     print(" ok")
-# #     print("parsing final")
-# print("done")
-
-
 
 print("start")
 print("getting links")
+links = set()
+links = get_all_articles__yahoo_finance()
+for link in links:
+    print (link)
 print("ok")
-print(" link parsing start")
-# https://finance.yahoo.com/news/federal-reserve-interest-rate-policy-decision-june-14-2023-180115494.html
-# https://news.yahoo.com/inside-hunt-idaho-killer-153751015.html
-link = 'https://finance.yahoo.com/news/federal-reserve-interest-rate-policy-decision-june-14-2023-180115494.html'
-print(link)
-print(" getting atribute_")
-atribute_ = get_article_info(link)
-print(" ok")
-print(atribute_)
-print(" getting ArticleInfo")
-ArticleInfo = parse_article__yahoo_finance(link)
-print(" ok")
-print(ArticleInfo[6])
-print(" saving file")
-save_to_disk(atribute_, ArticleInfo)
-print(" ok")
+for link in links:
+    print(" parsing start " + link)
+    print(" getting article_")
+    article_ = get_article_info(link)
+    print(" ok")
+    print(" getting ArticleInfo")
+    ArticleInfo = parse_article__yahoo_finance(link)
+    print(" ok")
+    print(" saving file")
+    save_to_disk(article_, ArticleInfo)
+    print(" ok")
+    print("parsing final")
 print("done")
+
+
+
+# print("start")
+# print("getting links")
+# print("ok")
+# print(" link parsing start")
+# # https://finance.yahoo.com/news/federal-reserve-interest-rate-policy-decision-june-14-2023-180115494.html
+# # https://news.yahoo.com/inside-hunt-idaho-killer-153751015.html
+# link = 'https://finance.yahoo.com/news/federal-reserve-interest-rate-policy-decision-june-14-2023-180115494.html'
+# print(link)
+# print(" getting atribute_")
+# atribute_ = get_article_info(link)
+# print(" ok")
+# print(atribute_)
+# print(" getting ArticleInfo")
+# ArticleInfo = parse_article__yahoo_finance(link)
+# print(" ok")
+# print(ArticleInfo[6])
+# print(" saving file")
+# save_to_disk(atribute_, ArticleInfo)
+# print(" ok")
+# print("done")
 
 #link = 'https://www.yahoo.com/news/san-francisco-police-9-victims-071629061.html'
 # url = 'https://www.yahoo.com/news/san-francisco-police-9-victims-071629061.html'
