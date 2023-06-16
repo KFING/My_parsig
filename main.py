4b5ca82fee0cf30419dbaf716095b82e0f551195
aliaksandr master <alxe.master@gmail.com>
2023-06-17 00:01:51 +0300

    cleanup the code

--- main.py
+++ main.py
@@ -1,141 +1,127 @@
 
-import os
-from bs4 import BeautifulSoup
-from urllib.request import urlopen
 import json
-import datetime
-from langdetect import detect
-import dateutil.parser as converter
 import lzma
-import base64
-import lxml
-import lxml.html
-import time
+import os
+from typing import NamedTuple, Dict, Any, Set
+
+from datetime import datetime
+
+import dateutil.parser as converter
 import requests
+from bs4 import BeautifulSoup
 
 
-class ContentParsingError(Exception):
-    def __init__(self, expression, message):
-        self.expression = expression
+CWD = os.getcwd()
+RESULTS_DIR = os.path.join(CWD, 'data')
+
+
+class ArticleInfo(NamedTuple):
+    title: str
+    text: str
+    publication_dt: datetime
+    parsing_dt: datetime
+    html: str
+    href: str
+    language: str
+
+    def to_dict(self) -> Dict[str, Any]:
+        dct = self._asdict()
+        dct['publication_dt'] = self.publication_dt.isoformat()
+        dct['parsing_dt'] = self.parsing_dt.isoformat()
+        return dct
+
+
+class ParsingError(Exception):
+    def __init__(self, message: str, exception: Exception) -> None:
+        self.exception = exception
         self.message = message
 
-def get_all_articles__yahoo_finance():
+
+class RequestError(Exception):
+    def __init__(self, message: str, exception: Exception) -> None:
+        self.exception = exception
+        self.message = message
+
+
+def get_all_articles__yahoo_finance() -> Set[str]:
     url = 'https://finance.yahoo.com'
-    str_ = 'https://finance.yahoo.com/news/'
     html_ = requests.get(url).content
     soup = BeautifulSoup(html_, 'html.parser')
     links = set()
     for link in soup.find_all('a'):
         l = link.get('href')
         if l != None and l.startswith('https'):
-            print(l)
             links.add(l)
-            # if (len(str(l)) > 31):
-            #     if (str_ == str(l)[:31]):
-            #         print(l)
-            #         links.add(l)
-    print("OK")
     return links
-#return links
 
-def get_article_info(url):
-    # page = urlopen(url)
-    # html_ = page.read()
-    # html_ = requests.get(url).content
-    # soup = BeautifulSoup(html_, 'html.parser')
-    # atribute_ = soup.find('article')
-    # return atribute_
+
+def get_article_id(url: str) -> str:
     j = 1
     ch = str(url)[len(str(url))-j]
     while ch != '/':
         j += 1
         ch = str(url)[len(str(url))-j]
-    article_ = str(url)[-(j-1):-5]
-    print(" OK")
     return str(url)[-(j-1):-5]
-#return article_
 
-def parse_article__yahoo_finance(url):
-    # page = urlopen(url)
-    # html_ = page.read().decode('utf-8')
+
+def parse_article__yahoo_finance(url: str) -> ArticleInfo:
     try:
-        html_ = requests.get(url).content
-        soup = BeautifulSoup(html_, 'html.parser')
-        attribute_ = soup.find("h1", class_="").text
-        publication_dt_ = converter.parse(soup.find("time").text).isoformat()
-        language_ = detect(attribute_)
+        response = requests.get(url)
+        response.raise_for_status()
+        html = response.content.decode('utf-8')
+    except Exception as e:
+        raise RequestError('wrong request', e)
+
+    try:
+        soup = BeautifulSoup(html, 'html.parser')
+        attribute_ = soup.find("h1").text
+        publication_dt_ = converter.parse(soup.find("time").text)
+        language_ = 'en'
         content_ = soup.find("div", class_="caas-body").text
         href_ = url
-        parsing_dt_ = datetime.datetime.now().replace(microsecond=0).isoformat()
-        ArticleInfo = (
-            str(attribute_),
-            str(content_),
-            str(publication_dt_),
-            str(parsing_dt_),
-            str(soup),
-            str(href_),
-            str(language_)
+        parsing_dt_ = datetime.now().replace(microsecond=0)
+        return ArticleInfo(
+            title=attribute_,
+            text=content_,
+            publication_dt=publication_dt_,
+            parsing_dt=parsing_dt_,
+            html=html,
+            href=href_,
+            language=language_,
         )
-    except AttributeError as e:
-        print (e)
-        return tuple()
-    else:
-        print(" OK")
-        return ArticleInfo
-#return ArticleInfo
+    except Exception as e:
+        raise ParsingError('wrong content of page', e)
 
-def save_to_disk(article_, ArticleInfo):
-    try:
-        os.makedirs('result_parsing/' + article_)
-        with lzma.open('result_parsing/' + article_ + '/' + article_ + '.xz', "w") as file:
-            file.write(ArticleInfo[4].encode('utf-8'))
-            file.close()
-        jsonFile = {
-            'header': ArticleInfo[0],
-            'content': ArticleInfo[1],
-            'publication_dt': ArticleInfo[2],
-            'parsing_dt': ArticleInfo[3],
-            'href': ArticleInfo[5],
-            'language': ArticleInfo[6]
-        }
-        #print(jsonFile)
-        with lzma.open('result_parsing/' + article_ + '/' + 'json' + article_ + '.xz', "w") as file:
-             file.write(str(jsonFile).encode('utf-8'))
-             file.close()
-             #print('t')
-        print(" OK")
-    except FileExistsError:
-        # directory already exists
-        pass
-#save
 
-def main():
+def save_to_disk(article_id: str, info: ArticleInfo) -> None:
+    os.makedirs(os.path.join(RESULTS_DIR, article_id), exist_ok=True)
+    with lzma.open(os.path.join(RESULTS_DIR, article_id, f'{article_id}.xz'), "wt") as file:
+        file.write(info.html)
+    with lzma.open(os.path.join(RESULTS_DIR, article_id, f'json{article_id}.xz'), "wb") as file:
+        file.write(json.dumps(info.to_dict()).encode('utf-8'))
+
+
+def main() -> None:
     print("start")
     print("getting links")
-    links = set()
     links = get_all_articles__yahoo_finance()
-    for link in links:
-        print (link)
+    print('\n'.join(links))
 
     for link in links:
         print(" parsing start " + link)
         print(" getting article_")
-        article_ = get_article_info(link)
+        article_id = get_article_id(link)
 
         print(" getting ArticleInfo")
-        ArticleInfo = parse_article__yahoo_finance(link)
-        if (len(ArticleInfo) == 0):continue
+        info = parse_article__yahoo_finance(link)
+        if len(info) == 0:
+            continue
         print(" saving file")
-        save_to_disk(article_, ArticleInfo)
+        save_to_disk(article_id, info)
 
         print(" parsing link final")
     print("done")
 
 
-def parser(time):
-    while True:
-        main()
-        time.sleep(3600/time)
-
-main()
-
+if __name__ == '__main__':
+    main()
