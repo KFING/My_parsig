52db342be68d70403c324fede66fff90594b22d5
Ehor Meschenko <meshhenko04@bk.ru>
2023-06-16 22:03:16 +0300

    upgrade

--- main.py
+++ main.py
@@ -1,22 +1,3 @@
-# import requests
-# import time
-# import csv
-# import re
-# from urllib.request import urlopen
-# from bs4 import BeautifulSoup
-#
-# #if __name__ == "__main__":
-#     str = 'https://news.yahoo.com/clarence-thomas-wrote-scathing-nearly-174813305.html'
-#     url = urlopen(str)
-#     link = url.read().decode('utf-8')
-#     soup = BeautifulSoup(link, 'html.parser')
-#     print(soup.find_all("h1", class_="caas-title-wrapper") )
-
-##get_all_articles__yahoo_finance(from_dt: Optional[datetime], to_dt: Optional[datetime]) -> Set[str]
-##get_article_info(href: str) -> str
-##parse_article__yahoo_finance(html: str) -> ArticleInfo
-#save_to_disk(file_name: str, article: ArticleInfo) -> None
-#https://www.yahoo.com/news/san-francisco-police-9-victims-071629061.html
 
 import os
 from bs4 import BeautifulSoup
@@ -31,6 +12,8 @@ import lxml
 import lxml.html
 import time
 import requests
+
+
 class ContentParsingError(Exception):
     def __init__(self, expression, message):
         self.expression = expression
@@ -147,56 +130,12 @@ def main():
 
         print(" parsing link final")
     print("done")
+
+
 def parser(time):
     while True:
         main()
         time.sleep(3600/time)
 
 main()
-
-
-# print("start")
-# print("getting links")
-# print("ok")
-# print(" link parsing start")
-# # https://finance.yahoo.com/news/federal-reserve-interest-rate-policy-decision-june-14-2023-180115494.html
-# # https://news.yahoo.com/inside-hunt-idaho-killer-153751015.html
-# link = 'https://finance.yahoo.com/news/federal-reserve-interest-rate-policy-decision-june-14-2023-180115494.html'
-# print(link)
-# print(" getting atribute_")
-# atribute_ = get_article_info(link)
-# print(" ok")
-# print(atribute_)
-# print(" getting ArticleInfo")
-# ArticleInfo = parse_article__yahoo_finance(link)
-# print(" ok")
-# print(ArticleInfo[6])
-# print(" saving file")
-# save_to_disk(atribute_, ArticleInfo)
-# print(" ok")
-# print("done")
-
-#link = 'https://www.yahoo.com/news/san-francisco-police-9-victims-071629061.html'
-# url = 'https://www.yahoo.com/news/san-francisco-police-9-victims-071629061.html'
-# page = urlopen(url)
-# html_ = page.read().decode('utf-8')
-# soup = BeautifulSoup(html_, 'html.parser')
-# atribute_ = soup.find("h1", class_="").text
-# publication_dt_ =  converter.parse(soup.find("time").text).isoformat()
-# language_ =  detect(atribute_)
-# content_ = soup.find("div", class_="caas-body").text
-# href_ = url
-# parsing_dt_ = datetime.datetime.now().replace(microsecond=0).isoformat()
-
-#print(soup)
-
-
-
-# links = set()
-# for link in soup.find_all('a'):
-#     l = link.get('href')
-#     if l != None and l.startswith('https'):
-#         links.add(l)
-# for link in links:
-#     print(link)
 
